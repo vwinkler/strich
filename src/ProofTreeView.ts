d2d3a6d888fdcb3129cb6606fb88ad631eeccdb4
Vincent Winkler <vwinkler@posteo.de>
2023-10-25 21:14:55 +0200

    Move inference line css out of style.css

--- src/ProofTreeView.ts
+++ src/ProofTreeView.ts
@@ -11,7 +11,7 @@ export function ProofTreeView(tree: ProofTree) : TemplateResult {
     return html`
     <div class="tree_container">
     <div class="premise_trees">${premiseTreeViews}</div>
-    <hr class="inference_line">
+    <hr class="inference_line" style="margin: 0">
     <div class="below_inference_line_container">
     ${tree.rule ? html`<div class="rule_name">${tree.rule}</div>` : html``}
     <div class="formula">${tree.conclusion}</div>
